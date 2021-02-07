4b412ad1a0c0eeb7e07ed3af28c75ca0b7641b0d
hemeshvpatel <hemesh.v.patel@gmail.com>
2021-02-06 20:06:50 -0600

    basic coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import importImg from "./noprobllama.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        color: "black",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "50px",
+      }}
+    >
+      <div>No Probllama Goods Coming Soon!</div>
+      <div>
+        <img src={importImg} alt="import"></img>
+      </div>
     </div>
   );
 }
